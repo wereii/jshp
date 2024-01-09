d9109f9672837fac9db0f346101ba263cea97108
Matyas Burnek <burnek.matyas@gmail.com>
2024-01-09 15:05:00 +0100

    added file handling, basic routing

---
+++ src/file_handler.rs
@@ -0,0 +1,59 @@
+use std::{path::PathBuf, fs::File};
+
+
+#[derive(Debug, Clone)]
+pub struct FileHandler {
+    root_dir: PathBuf
+}
+
+impl FileHandler {
+    pub fn new(path: &str) -> Result<Self, FileHandlerError>{
+        let root_dir = PathBuf::from(path);
+
+        if !root_dir.exists() || !root_dir.is_dir() {
+            return Err(FileHandlerError::InvalidServeDir(path.to_string()));
+        }
+
+        Ok(
+            Self{
+                root_dir,
+            }
+        )
+    }
+
+    pub fn get_file(&self, path: &str) -> Result<PathBuf, FileHandlerError> {
+        let mut file = self.root_dir.clone();
+        file.push(path);
+        
+        if !file.exists() || !file.is_file() {
+            return Err(FileHandlerError::FileDoesntExist(path.to_string()));
+        }
+
+        Ok(file)
+    }
+
+    pub fn read_to_string(&self, path: &str) -> Result<String, FileHandlerError> {
+        let file = self.get_file(path)?;
+        Ok(std::fs::read_to_string(file).expect("Existence should be already checked"))
+    }
+    
+    pub fn read_bytes(&self, path: &str) -> Result<Vec<u8>, FileHandlerError> {
+        let file = self.get_file(path)?;
+        Ok(std::fs::read(file).expect("Existence should be already checked"))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum FileHandlerError {
+    InvalidServeDir(String),
+    FileDoesntExist(String),
+}
+
+impl std::fmt::Display for FileHandlerError {
+    fn fmt(&self,f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            FileHandlerError::InvalidServeDir(e) => write!(f, "Invalid serve dir: {}", e),
+            FileHandlerError::FileDoesntExist(e) => write!(f, "File doesn't exist: {}", e),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,20 @@
 mod node_worker;
 mod parse;
 mod v8;
+mod file_handler;
 
-use std::io;
+use std::path::{Path, PathBuf};
+
 use astra::{Body, Response, ResponseBuilder, Server, Request};
-use log::error;
+use file_handler::{FileHandler, FileHandlerError};
+use log::{error, info};
 use pretty_env_logger;
 
 #[derive(Debug, Clone)]
 struct ServerState {
-    files: Vec<String>
+    file_handler: FileHandler,
 }
 
-fn locate_files(path: &str) -> io::Result<Vec<String>> {
-    let mut files = Vec::new();
-    let mut dirs = Vec::new();
-    dirs.push(path.to_string());
-    while let Some(dir) = dirs.pop() {
-        for entry in std::fs::read_dir(dir)? {
-            
-            let entry = entry.unwrap();
-            let entry_dir = entry.path();
-
-            if entry_dir.is_dir() {
-                dirs.push(entry_dir.to_str().unwrap().to_string());
-            } else {
-                let unix_path = format!("./{}/", path);
-                let windows_path = format!("{}\\", path);
-                let new_filepath = entry_dir
-                    .to_str().unwrap()
-                    .to_string()
-                    .replace(&unix_path, "")
-                    .replace(&windows_path, "");
-
-                files.push(new_filepath);
-            }
-        }
-    }
-    Ok(files)
-}
 
 fn server(address: &str, port: i32, state: ServerState) {
     println!("Listening on http://{}:{}", "localhost", 3000);
@@ -49,40 +25,73 @@ fn server(address: &str, port: i32, state: ServerState) {
 }
 
 
-fn route(req: Request, state: ServerState) -> Response {
+fn route(req: Request, ctx: ServerState) -> Response {
 
-    println!("Requested path: {}", req.uri());
+    info!("Requested page: {}", req.uri());
+    println!("Requested page: {}", req.uri());
 
-    let mut body = String::from("<h1>Available files</h1><br>");
+    // TODO? Kinda butchered
+    let mut stripped_path = req.uri().to_string();
 
-    for file in &state.files {
-        body += &format!("{}<br>", file);
+    if stripped_path.ends_with("/") {
+        stripped_path += "index.jshp";
     }
 
+    stripped_path.remove(0);
+
+    let buffer = match ctx.file_handler.read_bytes(&stripped_path) {
+        Ok(data) => data,
+        Err(_) => {
+            return ResponseBuilder::new()
+                .header("Content-Type", "text/html")
+                .body(Body::new("<h1>404 File not found</h1>"))
+                .unwrap();
+        }
+    };
+
+
+    let file_path = PathBuf::from(&stripped_path);
+    let content_type: String;
+
+    if let Some(extension) = file_path.extension() {
+        let extenstion = extension.to_str().expect("Should be convertible from OsStr to Str");
+
+        content_type = match extenstion {
+            "html" => "text/html",
+            "jpg" => "image/jpeg",
+            "webp" => "image/webp",
+            "png" => "image/png",
+            "svg" => "image/svg+xml",
+            _ => "text/plain",
+        }.to_string();
+    }
+    else {
+        content_type = String::from("text/plain");
+    }
+
+
     ResponseBuilder::new()
-        .header("Content-Type", "text/html")
-        .body(Body::new(body))
+        .header("Content-Type", content_type)
+        .body(Body::new(buffer))
         .unwrap()
 }
 
 fn main() {
     pretty_env_logger::init();
 
-    let files = match locate_files("./serve") {
-        Ok(v) => v,
+    let file_handler = match FileHandler::new("./serve") {
+        Ok(handler) => handler,
         Err(e) => {
-            error!("Failed to locate files: {}", e);
+            error!("FileHandler error: {}", e);
             std::process::exit(1);
         }
     };
 
-    for file in &files {
-        println!("{:?}", file);
-    }
+    let state = ServerState {
+        file_handler
+    };
 
-    server("127.0.0.1", 3000, ServerState{
-        files
-    });
+    server("127.0.0.1", 3000, state);
 }
 
 
